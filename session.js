ffce630d9bdb8ab799b09d8b1718db90b28882cc
Arunmozh <122620235+Arunmozh@users.noreply.github.com>
2023-01-18 22:43:17 +0800

    Update session.js

--- session.js
+++ session.js
@@ -1,39 +1,49 @@
-let url="test.json";
+let url="http:/"+"/localhost:8080/Testing%20/test.json";
 let data;
-
-/*
 fetch(url)
-.then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
-    }
-return response.json(); })
 
-  .then((text) =>
-      document.getElementById("result").innerText=text.name)
-  
-  .catch((error) =>       document.getElementById("result").innerText=error
-     )
-      
- */     
+
+
       
 async function loadNames() {
   const response = await fetch(url);
   const names = await response.json();
-  document.getElementById("result").innerText=names.age; 
+  document.getElementById("result").innerText=names.name;
   
 }
 loadNames();
 
 
-async function postName() {
+function saveToFirebase(email) {
+    var emailObject = {
+        email: email
+    };
+
+    firebase.database().ref('subscription-entries').push().set(emailObject)
+        .then(function(snapshot) {
+            success(); // some success method
+        }, function(error) {
+            document.getElementById("error").innerText=error;
+            error(); // some error method
+        });
+}
+
+saveToFirebase(email);
+
+
+
+
+
+
+
+
+/*async function postName() {
   const object = { name: 'James Gordon' };
   const response = await fetch(url, {
     method: 'POST',
     body: JSON.stringify(object)
   });
   const responseText = await response.text();
-document.getElementById("error").innerText=responseText;
   console.log(responseText); // logs 'OK'
 }
-postName();
+postName();*/
